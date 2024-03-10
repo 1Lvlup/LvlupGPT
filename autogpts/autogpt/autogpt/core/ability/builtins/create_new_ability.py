849b3ea984a1742673c7fb6105a7e9cb24b5e911
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:58:20 -0500

    improve

--- autogpts/autogpt/autogpt/core/ability/builtins/create_new_ability.py
+++ autogpts/autogpt/autogpt/core/ability/builtins/create_new_ability.py
@@ -1,107 +1,118 @@
 import logging
-from typing import ClassVar
+from dataclasses import dataclass
+from typing import Any, ClassVar, List, Literal, Optional
 
-from autogpt.core.ability.base import Ability, AbilityConfiguration
-from autogpt.core.ability.schema import AbilityResult
+import json_schema
+from autogpt.core.ability.base import Ability, AbilityConfiguration, AbilityResult
 from autogpt.core.plugin.simple import PluginLocation, PluginStorageFormat
-from autogpt.core.utils.json_schema import JSONSchema
+
+logger = logging.getLogger(__name__)
 
 
 class CreateNewAbility(Ability):
-    default_configuration = AbilityConfiguration(
+    default_configuration: AbilityConfiguration = AbilityConfiguration(
         location=PluginLocation(
             storage_format=PluginStorageFormat.INSTALLED_PACKAGE,
             storage_route="autogpt.core.ability.builtins.CreateNewAbility",
         ),
     )
 
-    def __init__(
-        self,
-        logger: logging.Logger,
-        configuration: AbilityConfiguration,
-    ):
-        self._logger = logger
-        self._configuration = configuration
+    @dataclass
+    class Configuration(AbilityConfiguration):
+        ability_name: str
+        description: str
+        arguments: List[dict[str, Any]]
+        required_arguments: List[str]
+        package_requirements: List[str]
+        code: str
 
     description: ClassVar[str] = "Create a new ability by writing python code."
 
-    parameters: ClassVar[dict[str, JSONSchema]] = {
-        "ability_name": JSONSchema(
+    parameters: ClassVar[dict[str, json_schema.JSONSchema]] = {
+        "ability_name": json_schema.JSONSchema(
             description="A meaningful and concise name for the new ability.",
-            type=JSONSchema.Type.STRING,
+            type=json_schema.JSONSchema.Type.STRING,
             required=True,
         ),
-        "description": JSONSchema(
+        "description": json_schema.JSONSchema(
             description=(
                 "A detailed description of the ability and its uses, "
                 "including any limitations."
             ),
-            type=JSONSchema.Type.STRING,
+            type=json_schema.JSONSchema.Type.STRING,
             required=True,
         ),
-        "arguments": JSONSchema(
+        "arguments": json_schema.JSONSchema(
             description="A list of arguments that the ability will accept.",
-            type=JSONSchema.Type.ARRAY,
-            items=JSONSchema(
-                type=JSONSchema.Type.OBJECT,
+            type=json_schema.JSONSchema.Type.ARRAY,
+            items=json_schema.JSONSchema(
+                type=json_schema.JSONSchema.Type.OBJECT,
                 properties={
-                    "name": JSONSchema(
+                    "name": json_schema.JSONSchema(
                         description="The name of the argument.",
-                        type=JSONSchema.Type.STRING,
+                        type=json_schema.JSONSchema.Type.STRING,
                     ),
-                    "type": JSONSchema(
+                    "type": json_schema.JSONSchema(
                         description=(
                             "The type of the argument. "
                             "Must be a standard json schema type."
                         ),
-                        type=JSONSchema.Type.STRING,
+                        type=json_schema.JSONSchema.Type.STRING,
                     ),
-                    "description": JSONSchema(
+                    "description": json_schema.JSONSchema(
                         description=(
                             "A detailed description of the argument and its uses."
                         ),
-                        type=JSONSchema.Type.STRING,
+                        type=json_schema.JSONSchema.Type.STRING,
                     ),
                 },
             ),
         ),
-        "required_arguments": JSONSchema(
+        "required_arguments": json_schema.JSONSchema(
             description="A list of the names of the arguments that are required.",
-            type=JSONSchema.Type.ARRAY,
-            items=JSONSchema(
+            type=json_schema.JSONSchema.Type.ARRAY,
+            items=json_schema.JSONSchema(
                 description="The names of the arguments that are required.",
-                type=JSONSchema.Type.STRING,
+                type=json_schema.JSONSchema.Type.STRING,
             ),
         ),
-        "package_requirements": JSONSchema(
+        "package_requirements": json_schema.JSONSchema(
             description=(
                 "A list of the names of the Python packages that are required to "
                 "execute the ability."
             ),
-            type=JSONSchema.Type.ARRAY,
-            items=JSONSchema(
+            type=json_schema.JSONSchema.Type.ARRAY,
+            items=json_schema.JSONSchema(
                 description=(
-                    "The of the Python package that is required to execute the ability."
+                    "The name of the Python package that is required to execute the ability."
                 ),
-                type=JSONSchema.Type.STRING,
+                type=json_schema.JSONSchema.Type.STRING,
             ),
         ),
-        "code": JSONSchema(
+        "code": json_schema.JSONSchema(
             description=(
                 "The Python code that will be executed when the ability is called."
             ),
-            type=JSONSchema.Type.STRING,
+            type=json_schema.JSONSchema.Type.STRING,
             required=True,
         ),
     }
+
+    def __init__(
+        self,
+        logger: logging.Logger,
+        configuration: Configuration,
+    ):
+        self._logger = logger
+        self._configuration = configuration
 
     async def __call__(
         self,
         ability_name: str,
         description: str,
-        arguments: list[dict],
-        required_arguments: list[str],
-        package_requirements: list[str],
+        arguments: List[dict[str, Any]],
+        required_arguments: List[str],
+        package_requirements: List[str],
         code: str,
     ) -> AbilityResult:
         raise NotImplementedError
