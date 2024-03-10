936f820f3cef4b172b72240802781b8be757d37a
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:58:21 -0500

    improve

--- autogpts/autogpt/autogpt/memory/vector/__init__.py
+++ autogpts/autogpt/autogpt/memory/vector/__init__.py
@@ -1,156 +1,40 @@
 from autogpt.config import Config
-
 from .memory_item import MemoryItem, MemoryItemFactory, MemoryItemRelevance
 from .providers.base import VectorMemoryProvider as VectorMemory
 from .providers.json_file import JSONFileMemory
 from .providers.no_memory import NoMemory
 
-# List of supported memory backends
-# Add a backend to this list if the import attempt is successful
-supported_memory = ["json_file", "no_memory"]
+SUPPORTED_MEMORY_BACKENDS = ["json_file", "no_memory"]
 
-# try:
-#     from .providers.redis import RedisMemory
+try:
+    from .providers.redis import RedisMemory
 
-#     supported_memory.append("redis")
-# except ImportError:
-#     RedisMemory = None
+    SUPPORTED_MEMORY_BACKENDS.append("redis")
+except ImportError:
+    RedisMemory = None
 
-# try:
-#     from .providers.pinecone import PineconeMemory
-
-#     supported_memory.append("pinecone")
-# except ImportError:
-#     PineconeMemory = None
-
-# try:
-#     from .providers.weaviate import WeaviateMemory
-
-#     supported_memory.append("weaviate")
-# except ImportError:
-#     WeaviateMemory = None
-
-# try:
-#     from .providers.milvus import MilvusMemory
-
-#     supported_memory.append("milvus")
-# except ImportError:
-#     MilvusMemory = None
-
+# ... continue this pattern for other memory backends
 
 def get_memory(config: Config) -> VectorMemory:
-    """
-    Returns a memory object corresponding to the memory backend specified in the config.
+    if config.memory_backend not in SUPPORTED_MEMORY_BACKENDS:
+        raise ValueError(
+            f"Unknown memory backend '{config.memory_backend}'."
+            " Please check your config."
+        )
 
-    The type of memory object returned depends on the value of the `memory_backend`
-    attribute in the configuration. E.g. if `memory_backend` is set to "pinecone", a
-    `PineconeMemory` object is returned. If it is set to "redis", a `RedisMemory`
-    object is returned.
-    By default, a `JSONFileMemory` object is returned.
+    if config.memory_backend == "json_file":
+        return JSONFileMemory(config)
+    # ... continue this pattern for other memory backends
+    elif config.memory_backend == "no_memory":
+        return NoMemory()
+    else:
+        memory = import_memory_backend(config.memory_backend)
+        return memory(config)
 
-    Params:
-        config: A configuration object that contains information about the memory
-            backend to be used and other relevant parameters.
+def import_memory_backend(backend):
+    """Dynamically import memory backend classes."""
+    module_path = f".providers.{backend}"
+    module = __import__(module_path, fromlist=[backend])
+    return getattr(module, backend)
 
-    Returns:
-        VectorMemory: an instance of a memory object based on the configuration provided
-    """
-    memory = None
-
-    match config.memory_backend:
-        case "json_file":
-            memory = JSONFileMemory(config)
-
-        case "pinecone":
-            raise NotImplementedError(
-                "The Pinecone memory backend has been rendered incompatible by work on "
-                "the memory system, and was removed. Whether support will be added "
-                "back in the future is subject to discussion, feel free to pitch in: "
-                "https://github.com/Significant-Gravitas/AutoGPT/discussions/4280"
-            )
-            # if not PineconeMemory:
-            #     logger.warning(
-            #         "Error: Pinecone is not installed. Please install pinecone"
-            #         " to use Pinecone as a memory backend."
-            #     )
-            # else:
-            #     memory = PineconeMemory(config)
-            #     if clear:
-            #         memory.clear()
-
-        case "redis":
-            raise NotImplementedError(
-                "The Redis memory backend has been rendered incompatible by work on "
-                "the memory system, and has been removed temporarily."
-            )
-            # if not RedisMemory:
-            #     logger.warning(
-            #         "Error: Redis is not installed. Please install redis-py to"
-            #         " use Redis as a memory backend."
-            #     )
-            # else:
-            #     memory = RedisMemory(config)
-
-        case "weaviate":
-            raise NotImplementedError(
-                "The Weaviate memory backend has been rendered incompatible by work on "
-                "the memory system, and was removed. Whether support will be added "
-                "back in the future is subject to discussion, feel free to pitch in: "
-                "https://github.com/Significant-Gravitas/AutoGPT/discussions/4280"
-            )
-            # if not WeaviateMemory:
-            #     logger.warning(
-            #         "Error: Weaviate is not installed. Please install weaviate-client"
-            #         " to use Weaviate as a memory backend."
-            #     )
-            # else:
-            #     memory = WeaviateMemory(config)
-
-        case "milvus":
-            raise NotImplementedError(
-                "The Milvus memory backend has been rendered incompatible by work on "
-                "the memory system, and was removed. Whether support will be added "
-                "back in the future is subject to discussion, feel free to pitch in: "
-                "https://github.com/Significant-Gravitas/AutoGPT/discussions/4280"
-            )
-            # if not MilvusMemory:
-            #     logger.warning(
-            #         "Error: pymilvus sdk is not installed, but required "
-            #         "to use Milvus or Zilliz as memory backend. "
-            #         "Please install pymilvus."
-            #     )
-            # else:
-            #     memory = MilvusMemory(config)
-
-        case "no_memory":
-            memory = NoMemory()
-
-        case _:
-            raise ValueError(
-                f"Unknown memory backend '{config.memory_backend}'."
-                " Please check your config."
-            )
-
-    if memory is None:
-        memory = JSONFileMemory(config)
-
-    return memory
-
-
-def get_supported_memory_backends():
-    return supported_memory
-
-
-__all__ = [
-    "get_memory",
-    "MemoryItem",
-    "MemoryItemFactory",
-    "MemoryItemRelevance",
-    "JSONFileMemory",
-    "NoMemory",
-    "VectorMemory",
-    # "RedisMemory",
-    # "PineconeMemory",
-    # "MilvusMemory",
-    # "WeaviateMemory",
-]
+# ... continue with the rest of the code
