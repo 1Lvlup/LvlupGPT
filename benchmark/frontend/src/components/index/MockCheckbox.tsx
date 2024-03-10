867c85b6af96ecf88cec7d442ec243a4c829b3ee
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:59:24 -0500

    improve

--- benchmark/frontend/src/components/index/MockCheckbox.tsx
+++ benchmark/frontend/src/components/index/MockCheckbox.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 
-import tw from "tailwind-styled-components";
+import tw, { css } from "tailwind-styled-components";
 
 interface MockCheckboxProps {
   isMock: boolean;
   setIsMock: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const MockCheckboxInput = tw.input`
+  border rounded
+  focus:border-blue-400 focus:ring focus:ring-blue-200 focus:ring-opacity-50
+  ${(props: any) =>
+    props.checked &&
+    css`
+      border-blue-400 bg-blue-100
+    `}
+`;
+
+const CheckboxWrapper = tw.label`
+  flex items-center space-x-2 mt-2
+  cursor-pointer
+`;
+
 const MockCheckbox: React.FC<MockCheckboxProps> = ({ isMock, setIsMock }) => {
   return (
-    <CheckboxWrapper>
+    <CheckboxWrapper htmlFor="mock-checkbox">
       <MockCheckboxInput
+        id="mock-checkbox"
         type="checkbox"
         checked={isMock}
         onChange={() => setIsMock(!isMock)}
+        required
       />
       <span>Run mock test</span>
     </CheckboxWrapper>
@@ -21,19 +38,3 @@ const MockCheckbox: React.FC<MockCheckboxProps> = ({ isMock, setIsMock }) => {
 };
 
 export default MockCheckbox;
-
-const MockCheckboxInput = tw.input`
-    border 
-    rounded 
-    focus:border-blue-400 
-    focus:ring 
-    focus:ring-blue-200 
-    focus:ring-opacity-50
-`;
-
-const CheckboxWrapper = tw.label`
-    flex 
-    items-center 
-    space-x-2 
-    mt-2
-`;
