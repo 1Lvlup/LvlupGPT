8ca6093bc6422daf4b458857803ddfc728f7c2ac
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:58:58 -0500

    improve

--- benchmark/agbenchmark/challenges/verticals/code/1_three_sum/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/verticals/code/1_three_sum/artifacts_out/sample_code.py
@@ -3,21 +3,31 @@ from typing import List, Optional
 
 
 def three_sum(nums: List[int], target: int) -> Optional[List[int]]:
-    nums_indices = [(num, index) for index, num in enumerate(nums)]
-    nums_indices.sort()
+    """
+    Given an array of integers nums and an integer target, return indices of the three numbers such that the sum equals target.
+
+    Args:
+        nums (List[int]): A list of integers.
+        target (int): An integer.
+
+    Returns:
+        Optional[List[int]]: A list of three indices, or None if no such triplet is found.
+    """
+    if not nums:
+        return None
+
+    nums_indices = sorted((num, index) for index, num in enumerate(nums))
     for i in range(len(nums_indices) - 2):
         if i > 0 and nums_indices[i] == nums_indices[i - 1]:
             continue
-        l, r = i + 1, len(nums_indices) - 1
-        while l < r:
-            three_sum = nums_indices[i][0] + nums_indices[l][0] + nums_indices[r][0]
-            if three_sum < target:
-                l += 1
-            elif three_sum > target:
-                r -= 1
+        left, right = i + 1, len(nums_indices) - 1
+        while left < right:
+            current_sum = nums_indices[i][0] + nums_indices[left][0] + nums_indices[right][0]
+            if current_sum < target:
+                left += 1
+            elif current_sum > target:
+                right -= 1
             else:
-                indices = sorted(
-                    [nums_indices[i][1], nums_indices[l][1], nums_indices[r][1]]
-                )
-                return indices
+                return [nums_indices[i][1], nums_indices[left][1], nums_indices[right][1]]
+
     return None
