f6bfccd724d2f3a17a3daa85a9255e6cb53ac9b7
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:58:13 -0500

    improve

--- autogpts/autogpt/autogpt/core/prompting/base.py
+++ autogpts/autogpt/autogpt/core/prompting/base.py
@@ -2,22 +2,9 @@ import abc
 
 from autogpt.core.configuration import SystemConfiguration
 from autogpt.core.resource.model_providers import AssistantChatMessage
-
 from .schema import ChatPrompt, LanguageModelClassification
 
-
 class PromptStrategy(abc.ABC):
-    default_configuration: SystemConfiguration
+    def __init__(self, default_configuration: SystemConfiguration):
+        self.default_configuration = default_configuration
 
-    @property
-    @abc.abstractmethod
-    def model_classification(self) -> LanguageModelClassification:
-        ...
-
-    @abc.abstractmethod
-    def build_prompt(self, *_, **kwargs) -> ChatPrompt:
-        ...
-
-    @abc.abstractmethod
-    def parse_response_content(self, response_content: AssistantChatMessage):
-        ...
