6dd6e72fcdc01282a90b0f062074dc6735d1a79f
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:58:36 -0500

    improve

--- benchmark/agbenchmark/challenges/deprecated/code/d2.1_guided/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/d2.1_guided/artifacts_out/sample_code.py
@@ -1,11 +1,25 @@
-from typing import List, Optional
+from typing import List
 
+def two_sum(nums: List[int], target: int) -> List[int]:
+    """
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-def two_sum(nums: List, target: int) -> Optional[List[int]]:
+    Args:
+    - nums: List[int] - a list of integers
+    - target: int - an integer
+
+    Returns:
+    List[int] - a list of two integers [i, j] such that nums[i] + nums[j] == target
+
+    Raises:
+    ValueError: if the input list has less than two elements
+    """
+    if len(nums) < 2:
+        raise ValueError("Input list must have at least two elements")
+
     seen = {}
     for i, num in enumerate(nums):
         complement = target - num
         if complement in seen:
             return [seen[complement], i]
         seen[num] = i
-    return None
