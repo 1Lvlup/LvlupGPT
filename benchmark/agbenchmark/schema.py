2eee2dca59811ae8bfb2a4d6815ecd1b33b6cb38
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:59:04 -0500

    improve

--- benchmark/agbenchmark/schema.py
+++ benchmark/agbenchmark/schema.py
@@ -4,20 +4,3 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-
-class TaskInput(BaseModel):
-    pass
-
-
-class TaskRequestBody(BaseModel):
-    input: str = Field(
-        ...,
-        min_length=1,
-        description="Input prompt for the task.",
-        example="Write the words you receive to the file 'output.txt'.",
-    )
-    additional_input: Optional[TaskInput] = {}
-
-
-class TaskEvalRequestBody(TaskRequestBody):
-    eval_id: str
