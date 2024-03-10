ccfebbacf9a3b81ebb9936bf486d00939a2720c6
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:59:23 -0500

    improve

--- benchmark/frontend/_eslintrc.cjs
+++ benchmark/frontend/_eslintrc.cjs
@@ -3,19 +3,21 @@ const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
+    ecmaVersion: 2020, // specify the ECMAScript version
+    sourceType: "module", // specify the source type
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "simple-import-sort"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:simple-import-sort/recommended", // add the simple-import-sort plugin
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -24,6 +26,8 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "simple-import-sort/imports": "warn", // add the simple-import-sort rule
+    "simple-import-sort/exports": "warn", // add the simple-import-sort rule
   },
 };
 
