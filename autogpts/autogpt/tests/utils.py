b695d006ac7145bdefab2c548f7262658a585910
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 18:58:20 -0500

    improve

--- autogpts/autogpt/tests/utils.py
+++ autogpts/autogpt/tests/utils.py
@@ -1,14 +1,10 @@
 import os
+import pathlib
 
 import pytest
 
 
-def skip_in_ci(test_function):
-    return pytest.mark.skipif(
-        os.environ.get("CI") == "true",
-        reason="This test doesn't work on GitHub Actions.",
-    )(test_function)
+@pytest.fixture
+def workspace():
+    return pathlib.Path().absolute()
 
-
-def get_workspace_file_path(workspace, file_name):
-    return str(workspace.get_path(file_name))
