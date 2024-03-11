d3710687c08f4f393f3719a1a3b2221a02d179ec
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:39 -0500

    comment

--- frontend/lib/models/task.dart
+++ frontend/lib/models/task.dart
@@ -1,12 +1,36 @@
 /// Represents a task or topic the user wants to discuss with the agent.
 class Task {
+  /// The unique identifier of the task.
   final String id;
+
+  /// Additional input provided by the user for the task.
+  /// This is an optional field and can be null.
   final Map<String, dynamic>? additionalInput;
+
+  /// Artifacts related to the task.
+  /// This is an optional field and can be null.
   final List<String>? artifacts;
+
+  /// The title of the task.
+  /// This field is nullable and can be set or retrieved using the [title] getter/setter.
   String? _title;
 
+  /// Constructor for the [Task] class.
+  ///
+  /// The [id] field is required, while [additionalInput] and [artifacts] are optional.
+  /// The [title] field is also optional and can be set later using the [title] setter.
+  Task({
+    required this.id,
+    this.additionalInput,
+    this.artifacts,
+  }) : _title = null;
+
+  /// Getter for the [title] field.
   String? get title => _title;
 
+  /// Setter for the [title] field.
+  ///
+  /// Throws an [ArgumentError] if the new title is empty.
   set title(String? newTitle) {
     if (newTitle?.isNotEmpty ?? false) {
       _title = newTitle;
@@ -15,7 +39,15 @@ class Task {
     }
   }
 
-  /// Converts a Map (usually from JSON) to a Task object
+  /// Converts a Map (usually from JSON) to a Task object.
+  ///
+  /// The Map should contain the following keys:
+  /// - `task_id`: The unique identifier of the task.
+  /// - `input`: The title of the task.
+  /// - `additional_input`: Additional input provided by the user for the task.
+  ///   This is an optional field and can be null.
+  /// - `artifacts`: Artifacts related to the task.
+  ///   This is an optional field and can be null.
   factory Task.fromMap(Map<String, dynamic> map) {
     Map<String, dynamic>? additionalInput;
     List<String>? artifacts;
@@ -30,34 +62,4 @@ class Task {
 
     final Task task = Task(
       id: map['task_id'],
-      additionalInput: additionalInput,
-      artifacts: artifacts,
-    );
-
-    task._title = map['input'];
-
-    return task;
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'task_id': id,
-      'input': _title,
-      'additional_input': additionalInput,
-      'artifacts': artifacts,
-    };
-  }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Task &&
-          runtimeType == other.runtimeType &&
-          id == other.id &&
-          _title == other._title &&
-          mapEquals(additionalInput, other.additionalInput) &&
-          listEquals(artifacts, other.artifacts);
-
-  @override
-  int get hashCode =>
-      id.hashCode ^ _title.hashCode ^ hashObjects(additionalInput)
+      additionalInput: additionalInput
