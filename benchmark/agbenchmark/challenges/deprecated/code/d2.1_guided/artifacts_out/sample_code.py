de89d454022b6c8969a05de299c18b297d62c816
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:00:47 -0500

    comment

--- benchmark/agbenchmark/challenges/deprecated/code/d2.1_guided/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/d2.1_guided/artifacts_out/sample_code.py
@@ -2,24 +2,30 @@ from typing import List
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     """
-    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    This function takes a list of integers `nums` and an integer `target` as input.
+    The goal is to find two numbers in `nums` that add up to the `target` value.
+    If such numbers are found, the function returns a list of their indices in the original `nums` list.
 
-    Args:
-    - nums: List[int] - a list of integers
-    - target: int - an integer
+    Parameters:
+    - nums (List[int]): a list of integers
+    - target (int): an integer
 
     Returns:
-    List[int] - a list of two integers [i, j] such that nums[i] + nums[j] == target
+    - List[int]: a list of two integers representing the indices of the two numbers in `nums` that add up to `target`
 
     Raises:
-    ValueError: if the input list has less than two elements
+    - ValueError: if the input list has less than two elements
     """
     if len(nums) < 2:
+        # The function requires at least two elements in the input list to find a pair of numbers that add up to the target.
+        # If the list has less than two elements, a ValueError is raised.
         raise ValueError("Input list must have at least two elements")
 
-    seen = {}
+    seen = {}  # A dictionary to store the numbers we've seen so far and their indices.
     for i, num in enumerate(nums):
-        complement = target - num
+        # Iterate through the input list and its elements.
+        complement = target - num  # Calculate the complement of the current number.
         if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
+            # If the complement is in the dictionary, it means we've found a pair of numbers that add up to the target.
+            return [seen[complement], i]  # Return their indices.
+        seen[num] = i  # If the complement is not found, add the current number and its index to the dictionary.
