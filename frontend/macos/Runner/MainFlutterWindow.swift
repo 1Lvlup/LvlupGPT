d4c04994a9e535de7bfc7ede23d51196609cb4f8
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:43 -0500

    comment

--- frontend/macos/Runner/MainFlutterWindow.swift
+++ frontend/macos/Runner/MainFlutterWindow.swift
@@ -1,15 +1,24 @@
 import Cocoa
 import FlutterMacOS
 
+// Define a MainFlutterWindow class that inherits from NSWindow
 class MainFlutterWindow: NSWindow {
+  
+  // Override the awakeFromNib method to perform additional configuration
   override func awakeFromNib() {
+    
+    // Create a new FlutterViewController instance
     let flutterViewController = FlutterViewController()
+    
+    // Get the current window frame and set it as the frame for the FlutterViewController
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    
+    // Register any generated plugins with the FlutterViewController
     RegisterGeneratedPlugins(registry: flutterViewController)
-
+    
+    // Call the superclass's awakeFromNib method
     super.awakeFromNib()
   }
 }
