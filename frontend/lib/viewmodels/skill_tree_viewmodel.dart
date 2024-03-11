7fb2f2c5951663ee11d40a3a8061136207886d5e
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:44 -0500

    comment

--- frontend/lib/viewmodels/skill_tree_viewmodel.dart
+++ frontend/lib/viewmodels/skill_tree_viewmodel.dart
@@ -3,29 +3,46 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:graphview/GraphView.dart';
 
+// Import custom models for the skill tree
 import 'package:auto_gpt_flutter_client/models/skill_tree/skill_tree_category.dart';
 import 'package:auto_gpt_flutter_client/models/skill_tree/skill_tree_edge.dart';
 import 'package:auto_gpt_flutter_client/models/skill_tree/skill_tree_node.dart';
 
+/// A ViewModel for managing the SkillTree data and UI state
 class SkillTreeViewModel extends ChangeNotifier {
+  // Private list of SkillTreeNodes
   List<SkillTreeNode> _skillTreeNodes = [];
+
+  /// List of SkillTreeNodes
   List<SkillTreeNode> get skillTreeNodes => _skillTreeNodes;
 
+  // Private list of SkillTreeEdges
   List<SkillTreeEdge> _skillTreeEdges = [];
+
+  /// List of SkillTreeEdges
   List<SkillTreeEdge> get skillTreeEdges => _skillTreeEdges;
 
+  // Selected SkillTreeNode
   SkillTreeNode? _selectedNode;
+
+  /// Selected SkillTreeNode
   SkillTreeNode? get selectedNode => _selectedNode;
 
+  /// Graph object for visualizing the skill tree
   final Graph graph = Graph();
+
+  /// Configuration for the Sugiyama layout algorithm
   SugiyamaConfiguration builder = SugiyamaConfiguration();
 
+  /// Current SkillTreeCategory
   SkillTreeCategory currentSkillTreeType = SkillTreeCategory.general;
 
+  /// Initialize the SkillTree by reading JSON data from assets
   Future<void> initializeSkillTree() async {
     try {
       resetState();
 
+      // Determine the JSON file name based on the current skill tree type
       String fileName = currentSkillTreeType.jsonFileName;
 
       // Read the JSON file from assets
@@ -46,6 +63,7 @@ class SkillTreeViewModel extends ChangeNotifier {
         _skillTreeEdges.add(edge);
       }
 
+      // Configure the Sugiyama layout algorithm
       builder.orientation = (SugiyamaConfiguration.ORIENTATION_LEFT_RIGHT);
       builder.bendPointShape = CurvedBendPointShape(curveLength: 20);
 
@@ -57,12 +75,14 @@ class SkillTreeViewModel extends ChangeNotifier {
     }
   }
 
+  /// Reset the SkillTree state
   void resetState() {
     _skillTreeNodes = [];
     _skillTreeEdges = [];
     _selectedNode = null;
   }
 
+  /// Toggle the selection of a SkillTreeNode
   void toggleNodeSelection(String nodeId) {
     if (_selectedNode?.id == nodeId) {
       // Unselect the node if it's already selected
@@ -74,7 +94,7 @@ class SkillTreeViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  // Function to get a node by its ID
+  /// Get a SkillTreeNode by its ID
   SkillTreeNode? getNodeById(String nodeId) {
     try {
       // Find the node in the list where the ID matches
@@ -85,3 +105,4 @@ class SkillTreeViewModel extends ChangeNotifier {
     }
   }
 }
+
