22c5ab7f725a57999285b9102e64447005eea005
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:00:41 -0500

    comment

--- benchmark/agbenchmark/challenges/deprecated/code/1_return/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/1_return/artifacts_out/sample_code.py
@@ -1,9 +1,12 @@
 def multiply_number(number: float) -> float:
     """
-    This function takes a number (accepts both int and float) as input and returns the result of multiplying it by 2.
+    This function takes a single numerical argument, `number`, and returns the result of multiplying it by 2.
+    While the function is defined with a float input type, it can also accept integer inputs.
 
     :param number: the number to be multiplied
+    :type number: float or int
     :return: the multiplied number
+    :rtype: float
     """
     multiplied_number = number * 2
     return multiplied_number
