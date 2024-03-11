accedb3a2cef5c81c1c1735754f64420c4812741
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:41 -0500

    comment

--- frontend/linux/main.cc
+++ frontend/linux/main.cc
@@ -1,6 +1,11 @@
 #include "my_application.h"
 
-int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = my_application_new();
-  return g_application_run(G_APPLICATION(app), argc, argv);
-}
+// Create a new instance of MyApplication using the g_autoptr() convenience wrapper.
+// This ensures that the memory allocated for the app will be automatically freed when it goes out of scope.
+g_autoptr(MyApplication) app = my_application_new();
+
+// Call the g_application_run() function to start the application's event loop and execute its main() function.
+// The function will not return until the application has been terminated.
+// The argc and argv arguments are passed directly to the underlying GApplication's main() function.
+return g_application_run(G_APPLICATION(app), argc, argv);
+
