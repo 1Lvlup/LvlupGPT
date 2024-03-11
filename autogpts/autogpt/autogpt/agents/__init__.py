8d41dc1fb5dd07e17b7156cd8c53fc1a94d5232b
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:00:21 -0500

    comment

--- autogpts/autogpt/autogpt/agents/__init__.py
+++ autogpts/autogpt/autogpt/agents/__init__.py
@@ -1,4 +1,9 @@
 from .agent import Agent
 from .base import AgentThoughts, BaseAgent, CommandArgs, CommandName
 
+# The __all__ variable is a list of module-level variables that will be imported when
+# this module is imported using the 'from module import *' syntax. In this case,
+# we want to make the BaseAgent, Agent, CommandName, CommandArgs, and AgentThoughts
+# classes available for import.
 __all__ = ["BaseAgent", "Agent", "CommandName", "CommandArgs", "AgentThoughts"]
+
