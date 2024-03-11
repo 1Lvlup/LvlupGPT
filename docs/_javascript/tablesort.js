a867fd2fa696d3f8588e970df0544a699f4c860e
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:26 -0500

    comment

--- docs/_javascript/tablesort.js
+++ docs/_javascript/tablesort.js
@@ -1,6 +1,11 @@
+// Subscribes to the document$ observable stream
 document$.subscribe(function () {
-    var tables = document.querySelectorAll("article table:not([class])")
+    // Selects all <table> elements within <article> elements that do not have a defined class attribute
+    var tables = document.querySelectorAll("article table:not([class])");
+
+    // Iterates over the selected <table> elements using the forEach method
     tables.forEach(function (table) {
-        new Tablesort(table)
-    })
-})
+        // For each <table> element, a new instance of the Tablesort class is created and initialized with the current <table> element as its argument
+        new Tablesort(table);
+    });
+});
