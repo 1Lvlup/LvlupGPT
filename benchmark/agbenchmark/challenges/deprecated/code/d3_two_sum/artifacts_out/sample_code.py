da2e1eeffdb05ff5225800fdc70ac7787a4f6ed1
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:00:50 -0500

    comment

--- benchmark/agbenchmark/challenges/deprecated/code/d3_two_sum/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/d3_two_sum/artifacts_out/sample_code.py
@@ -1,11 +1,8 @@
-from typing import List, Optional
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))  # Output: [0, 1]
 
 
-def two_sum(nums: List, target: int) -> Optional[List[int]]:
-    seen = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
-    return None
+nums = [3, 2, 4, 2]
+target = 6
+print(two_sum(nums, target))  # Output: [1, 2]
