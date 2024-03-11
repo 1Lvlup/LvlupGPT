f5220cf08b8b6731555bf334ba1b69962cd66a38
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:47 -0500

    comment

--- frontend/windows/runner/main.cpp
+++ frontend/windows/runner/main.cpp
@@ -5,10 +5,12 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+// The entry point for the Windows application. This function is called when
+// the application starts.
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
+  // Attach to console when present (e.g., 'flutter run') or create a new
+  // console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
@@ -17,27 +19,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
+  // Create a new DartProject object with the specified data directory.
   flutter::DartProject project(L"data");
 
+  // Parse the command line arguments and set them as the Dart entrypoint
+  // arguments for the project.
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
-
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
+  // Create a new FlutterWindow object with the specified project.
   FlutterWindow window(project);
+
+  // Set the origin and size of the window.
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
+
+  // Create the window with the specified title, origin, and size.
   if (!window.Create(L"auto_gpt_flutter_client", origin, size)) {
     return EXIT_FAILURE;
   }
-  window.SetQuitOnClose(true);
 
-  ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
-    ::TranslateMessage(&msg);
-    ::DispatchMessage(&msg);
-  }
-
-  ::CoUninitialize();
-  return EXIT_SUCCESS;
-}
+  // Set the window to quit when closed.
+  window.SetQuitOnClose(
