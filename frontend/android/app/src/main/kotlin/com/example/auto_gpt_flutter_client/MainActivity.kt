128dca8509e0222c00a0bd694b9a49d0fcb30e15
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:28 -0500

    comment

--- frontend/android/app/src/main/kotlin/com/example/auto_gpt_flutter_client/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/auto_gpt_flutter_client/MainActivity.kt
@@ -2,5 +2,20 @@ package com.example.auto_gpt_flutter_client
 
 import io.flutter.embedding.android.FlutterActivity
 
+/**
+ * The main activity for the AutoGPT Flutter client application.
+ * This class extends FlutterActivity, which provides the necessary integration
+ * with the Flutter engine to run the app.
+ */
 class MainActivity: FlutterActivity() {
+    // Alternatively, if there are any specific methods or properties in this class
+    // that you'd like to document, you can add comments there as well.
+    // For example:
+    //
+    // /**
+    //  * Called when the user taps the "+" button in the app bar.
+    //  */
+    // fun onAddButtonTapped() {
+    //     // Code to handle button tap goes here
+    // }
 }
