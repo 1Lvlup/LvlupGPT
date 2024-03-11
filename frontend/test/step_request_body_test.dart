6f8fbecaa2b0328c141638c49b5d3efaee7dbbf8
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:45 -0500

    comment

--- frontend/test/step_request_body_test.dart
+++ frontend/test/step_request_body_test.dart
@@ -2,24 +2,32 @@ import 'package:auto_gpt_flutter_client/models/step_request_body.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  // Begin a new test group for the StepRequestBody class
   group('StepRequestBody', () {
+    // Define a test to check if StepRequestBody creates an instance with correct values
     test('should create StepRequestBody with correct values', () {
+      // Create an instance of StepRequestBody with input and additionalInput
       final stepRequestBody = StepRequestBody(
           input: 'Execute something', additionalInput: {'key': 'value'});
 
+      // Assert that the input and additionalInput values are as expected
       expect(stepRequestBody.input, 'Execute something');
       expect(stepRequestBody.additionalInput, {'key': 'value'});
     });
 
+    // Define a test to check if StepRequestBody can be converted to the correct JSON
     test('should convert StepRequestBody to correct JSON', () {
+      // Create an instance of StepRequestBody with input and additionalInput
       final stepRequestBody = StepRequestBody(
           input: 'Execute something', additionalInput: {'key': 'value'});
 
+      // Convert the StepRequestBody instance to a JSON map
       final json = stepRequestBody.toJson();
 
+      // Assert that the JSON map has the expected structure
       expect(json, {
-        'input': 'Execute something',
-        'additional_input': {'key': 'value'}
+        'input': 'Execute something', // The input value
+        'additional_input': {'key': 'value'}, // The additionalInput value
       });
     });
   });
