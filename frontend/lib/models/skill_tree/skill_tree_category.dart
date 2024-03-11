6dede95ac71abaa2db42f6f1a78e56d1a9649885
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:43 -0500

    comment

--- frontend/lib/models/skill_tree/skill_tree_category.dart
+++ frontend/lib/models/skill_tree/skill_tree_category.dart
@@ -1,11 +1,9 @@
-enum SkillTreeCategory {
-  general,
-  coding,
-  data,
-  scrapeSynthesize,
-}
+// Define an enum `SkillTreeCategory` with four categories: general, coding, data, and scrapeSynthesize.
+enum SkillTreeCategory { general, coding, data, scrapeSynthesize }
 
+// Extension of the `SkillTreeCategory` enum, providing additional functionality.
 extension SkillTreeTypeExtension on SkillTreeCategory {
+  // A map that maps each category to its string representation.
   Map<SkillTreeCategory, String> _stringValues = {
     SkillTreeCategory.general: 'General',
     SkillTreeCategory.coding: 'Coding',
@@ -13,14 +11,9 @@ extension SkillTreeTypeExtension on SkillTreeCategory {
     SkillTreeCategory.scrapeSynthesize: 'Scrape/Synthesize',
   };
 
+  // A map that maps each category to the name of its corresponding JSON file.
   Map<SkillTreeCategory, String> _jsonFileNames = {
     SkillTreeCategory.general: 'general_tree_structure.json',
     SkillTreeCategory.coding: 'coding_tree_structure.json',
     SkillTreeCategory.data: 'data_tree_structure.json',
-    SkillTreeCategory.scrapeSynthesize: 'scrape_synthesize_tree_structure.json',
-  };
-
-  String get stringValue => _stringValues[this] ?? '';
-
-  String get jsonFileName => _jsonFileNames[this] ?? '';
-}
+    SkillTreeCategory.scrapeSynthesize: 'scrape_synthesize
