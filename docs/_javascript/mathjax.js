ffc76318dfe7125f92a7aafe42709ce2e8e7d0c1
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:24 -0500

    comment

--- docs/_javascript/mathjax.js
+++ docs/_javascript/mathjax.js
@@ -1,18 +1,26 @@
+// Wait for the DOM to be fully loaded before executing the code
 document.addEventListener('DOMContentLoaded', () => {
+
+  // Define the configuration for MathJax
   window.MathJax = {
     tex: {
+      // Configuration for inline and display math equations
       inlineMath: [['\\(', '\\)']],
       displayMath: [['\\[', '\\]']],
-      processEscapes: true,
-      processEnvironments: true
+      processEscapes: true, // Enable processing of escaped characters
+      processEnvironments: true // Enable processing of LaTeX environments
     },
     options: {
+      // Configuration for ignoring and processing HTML classes
       ignoreHtmlClass: '.*|',
       processHtmlClass: 'arithmatex'
     }
   };
 
-  MathJax.typesetPromise().catch(error => {
-    console.error('MathJax failed to typeset: ', error);
-  });
+  // Call the MathJax typesetPromise method to render the math equations
+  MathJax.typesetPromise()
+    .catch(error => {
+      // If there's an error during typesetting, log the error to the console
+      console.error('MathJax failed to typeset: ', error);
+    });
 });
