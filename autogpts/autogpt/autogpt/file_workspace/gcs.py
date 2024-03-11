535a2cc365e7334c0eb8773a77cd2374dda64816
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:00:41 -0500

    comment

--- autogpts/autogpt/autogpt/file_workspace/gcs.py
+++ autogpts/autogpt/autogpt/file_workspace/gcs.py
@@ -14,21 +14,31 @@ from google.cloud.exceptions import NotFound
 
 from autogpt.core.configuration.schema import UserConfigurable
 
-from .base import FileWorkspace, FileWorkspaceConfiguration
-
 logger = logging.getLogger(__name__)
 
 
 class GCSFileWorkspaceConfiguration(FileWorkspaceConfiguration):
+    """A class representing the configuration for a Google Cloud Storage file workspace.
+
+    Attributes:
+        bucket (str): The name of the Google Cloud Storage bucket to use for the workspace.
+    """
     bucket: str = UserConfigurable("autogpt", from_env="WORKSPACE_STORAGE_BUCKET")
 
 
 class GCSFileWorkspace(FileWorkspace):
-    """A class that represents a Google Cloud Storage workspace."""
+    """A class that represents a Google Cloud Storage workspace.
 
-    _bucket: storage.Bucket
+    Attributes:
+        _bucket (storage.Bucket): The Google Cloud Storage bucket used for the workspace.
+    """
 
     def __init__(self, config: GCSFileWorkspaceConfiguration):
+        """Initialize the GCSFileWorkspace instance.
+
+        Args:
+            config (GCSFileWorkspaceConfiguration): The configuration for the workspace.
+        """
         self._bucket_name = config.bucket
         self._root = config.root
         assert self._root.is_absolute()
@@ -38,15 +48,25 @@ class GCSFileWorkspace(FileWorkspace):
 
     @property
     def root(self) -> Path:
-        """The root directory of the file workspace."""
+        """Return the root directory of the file workspace.
+
+        Returns:
+            Path: The root directory of the file workspace.
+        """
         return self._root
 
     @property
     def restrict_to_root(self) -> bool:
-        """Whether to restrict generated paths to the root."""
+        """Return whether to restrict generated paths to the root.
+
+        Returns:
+            bool: True if paths should be restricted to the root, False otherwise.
+        """
         return True
 
     def initialize(self) -> None:
+        """Initialize the workspace by creating or getting the Google Cloud Storage bucket.
+        """
         logger.debug(f"Initializing {repr(self)}...")
         try:
             self._bucket = self._gcs.get_bucket(self._bucket_name)
@@ -55,24 +75,62 @@ class GCSFileWorkspace(FileWorkspace):
             self._bucket = self._gcs.create_bucket(self._bucket_name)
 
     def get_path(self, relative_path: str | Path) -> Path:
+        """Return the Path object for the given relative path, relative to the root.
+
+        Args:
+            relative_path (str or Path): The relative path to the file or directory.
+
+        Returns:
+            Path: The Path object for the given relative path.
+        """
         return super().get_path(relative_path).relative_to("/")
 
     def _get_blob(self, path: str | Path) -> storage.Blob:
+        """Return the Google Cloud Storage Blob object for the given path.
+
+        Args:
+            path (str or Path): The path to the file or directory.
+
+        Returns:
+            storage.Blob: The Google Cloud Storage Blob object for the given path.
+        """
         path = self.get_path(path)
         return self._bucket.blob(str(path))
 
     def open_file(self, path: str | Path, binary: bool = False) -> IOBase:
-        """Open a file in the workspace."""
+        """Open a file in the workspace.
+
+        Args:
+            path (str or Path): The path to the file.
+            binary (bool): Whether to open the file in binary mode.
+
+        Returns:
+            IOBase: A file object that can be used to read or write to the file.
+        """
         blob = self._get_blob(path)
         blob.reload()  # pin revision number to prevent version mixing while reading
         return blob.open("rb" if binary else "r")
 
     def read_file(self, path: str | Path, binary: bool = False) -> str | bytes:
-        """Read a file in the workspace."""
+        """Read the contents of a file in the workspace.
+
+        Args:
+            path (str or Path): The path to the file.
+            binary (bool): Whether to read the file in binary mode.
+
+        Returns:
+            str or bytes: The contents of the file.
+        """
         return self.open_file(path, binary).read()
 
     async def write_file(self, path: str | Path, content: str | bytes) -> None:
-        """Write to a file in the workspace."""
+        """Write the given content to a file in the workspace.
+
+        Args:
+            path (str or Path): The path to the file.
+            content (str or bytes): The content to write to the file.
+
+        """
         blob = self._get_blob(path)
 
         blob.upload_from_string(
@@ -94,7 +152,14 @@ class GCSFileWorkspace(FileWorkspace):
                 await res
 
     def list(self, path: str | Path = ".") -> list[Path]:
-        """List all files (recursively) in a directory in the workspace."""
+        """List all files (recursively) in a directory in the workspace.
+
+        Args:
+            path (str or Path, optional): The path to the directory. Defaults to ".".
+
+        Returns:
+            list[Path]: A list of Path objects for the files in the directory.
+        """
         path = self.get_path(path)
         return [
             Path(blob.name).relative_to(path)
@@ -104,10 +169,6 @@ class GCSFileWorkspace(FileWorkspace):
         ]
 
     def delete_file(self, path: str | Path) -> None:
-        """Delete a file in the workspace."""
-        path = self.get_path(path)
-        blob = self._bucket.blob(str(path))
-        blob.delete()
+        """Delete a file in the workspace.
 
-    def __repr__(self) -> str:
-        return f"{__class__.__name__}(bucket='{self._bucket_name}', root={self._root})"
+       
