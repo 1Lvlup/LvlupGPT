2340fc51bcab390ee8d63d88a65bb7cd7a2ad919
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:01:26 -0500

    comment

--- benchmark/frontend/prettier.config.cjs
+++ benchmark/frontend/prettier.config.cjs
@@ -1,10 +1,11 @@
-// Importing Prettier's config type
+// Importing Prettier's config type from the 'prettier' module
 const { Config } = require("prettier");
 
-// Creating the Prettier config object
+// Creating a Prettier config object and setting its plugins property to an array
+// that includes the 'prettier-plugin-tailwindcss' module
 const config: Config = {
   plugins: [require("prettier-plugin-tailwindcss")],
 };
 
-// Exporting the config object
+// Exporting the config object as the default export of this module
 module.exports = config;
