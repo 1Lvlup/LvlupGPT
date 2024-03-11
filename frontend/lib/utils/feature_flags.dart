67a7578993002e549975408a255a619a1b96cbde
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:38 -0500

    comment

--- frontend/lib/utils/feature_flags.dart
+++ frontend/lib/utils/feature_flags.dart
@@ -1,3 +1,12 @@
+// This is the FeatureFlags class, which contains a set of static constants
+// that represent various feature flags for the application. Each feature
+// flag is a boolean value that can be enabled or disabled.
+//
+// In this example, the FeatureFlags class contains a single feature flag
+// named `userExperienceIterationTwoEnabled`. This flag represents whether
+// or not the second iteration of the application's user experience is
+// enabled. Currently, this flag is set to false, indicating that the second
+// iteration of the user experience is disabled.
 class FeatureFlags {
   static const bool userExperienceIterationTwoEnabled = false;
 }
