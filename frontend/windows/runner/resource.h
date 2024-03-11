ae3bdcec624643ffa43d84ccb429f35e803fcfef
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:01:28 -0500

    improve

--- frontend/windows/runner/resource.h
+++ frontend/windows/runner/resource.h
@@ -1,16 +1,21 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Runner.rc
-//
+
+// This is a Microsoft Visual C++ generated include file.
+// It is used by Runner.rc, which is part of the Resource file.
+
+// Resource IDs for the application icon and other resources.
 #define IDI_APP_ICON                    101
 
-// Next default values for new objects
-//
+// Default values for new objects in the resource file.
 #ifdef APSTUDIO_INVOKED
+
+// If the APSTUDIO_READONLY_SYMBOLS macro is defined, then the values below
+// are used for the next resource, command, control, and symbol.
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#endif // APSTUDIO_READONLY_SYMBOLS
+
+#endif // APSTUDIO_INVOKED
