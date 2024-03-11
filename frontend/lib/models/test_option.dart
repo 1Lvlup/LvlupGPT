433cb0d461ec319e2aa070e36d8752efdf9c3dbf
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:41 -0500

    comment

--- frontend/lib/models/test_option.dart
+++ frontend/lib/models/test_option.dart
@@ -1,4 +1,5 @@
-/// `TestOption` is an enumeration of the available test options that can be selected in the skill tree view.
+/// `TestOption` is an enumeration of the available test options that can be
+/// selected in the skill tree view.
 ///
 /// Each value of this enum represents a distinct test option that can be executed.
 /// The `description` getter can be used to get the string representation of each test option.
@@ -13,31 +14,35 @@ enum TestOption {
   runAllTestsInCategory,
 }
 
-/// An extension on the `TestOption` enum to provide a string representation for each test option.
+/// An extension on the `TestOption` enum to provide a string representation for
+/// each test option.
 ///
-/// This extension adds a `description` getter on `TestOption` to easily retrieve the human-readable
-/// string associated with each option. This is particularly helpful for UI display purposes.
+/// This extension adds a `description` getter on `TestOption` to easily retrieve
+/// the human-readable string associated with each option. This is particularly
+/// helpful for UI display purposes.
 extension TestOptionExtension on TestOption {
   /// Gets the string representation of the test option.
   ///
-  /// Returns a human-readable string that describes the test option. This string is intended
-  /// to be displayed in the UI for user selection.
+  /// Returns a human-readable string that describes the test option. This string
+  /// is intended to be displayed in the UI for user selection.
   String get description {
     switch (this) {
       /// In case of a single test option, return the corresponding string.
       case TestOption.runSingleTest:
         return 'Run single test';
 
-      /// In case of a test suite option that includes selected node and ancestors, return the corresponding string.
+      /// In case of a test suite option that includes selected node and ancestors,
+      /// return the corresponding string.
       case TestOption.runTestSuiteIncludingSelectedNodeAndAncestors:
         return 'Run test suite including selected node and ancestors';
 
-      /// In case of an option to run all tests in a category, return the corresponding string.
+      /// In case of an option to run all tests in a category, return the corresponding
+      /// string.
       case TestOption.runAllTestsInCategory:
         return 'Run all tests in category';
 
-      /// In case of an undefined or unknown test option, return a generic unknown string.
-      /// This case should ideally never be hit if all enum values are handled.
+      /// In case of an undefined or unknown test option, return a generic unknown
+      /// string. This case should ideally never be hit if all enum values are handled.
       default:
         return 'Unknown';
     }
