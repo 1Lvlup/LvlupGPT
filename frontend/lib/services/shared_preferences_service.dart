f7765f308dac56e5ffd76f3dd0d115aad8ffc955
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:01:24 -0500

    improve

--- frontend/lib/services/shared_preferences_service.dart
+++ frontend/lib/services/shared_preferences_service.dart
@@ -6,101 +6,23 @@ class SharedPreferencesService {
   static final SharedPreferencesService instance =
       SharedPreferencesService._privateConstructor();
 
-  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late SharedPreferences _prefs;
 
-  /// Sets a boolean [value] for the given [key] in the shared preferences.
-  ///
-  /// Example:
-  /// ```dart
-  /// await prefsService.setBool('isLoggedIn', true);
-  /// ```
-  Future<void> setBool(String key, bool value) async {
-    final prefs = await _prefs;
-    prefs.setBool(key, value);
-  }
+  Future<void> initialize() async => _prefs = await SharedPreferences.getInstance();
 
-  /// Sets a string [value] for the given [key] in the shared preferences.
-  ///
-  /// Example:
-  /// ```dart
-  /// await prefsService.setString('username', 'Alice');
-  /// ```
-  Future<void> setString(String key, String value) async {
-    final prefs = await _prefs;
-    prefs.setString(key, value);
-  }
+  Future<void> setBool(String key, bool value) async => _prefs.setBool(key, value);
 
-  /// Sets an integer [value] for the given [key] in the shared preferences.
-  ///
-  /// Example:
-  /// ```dart
-  /// await prefsService.setInt('age', 30);
-  /// ```
-  Future<void> setInt(String key, int value) async {
-    final prefs = await _prefs;
-    prefs.setInt(key, value);
-  }
+  Future<void> setString(String key, String value) async => _prefs.setString(key, value);
 
-  /// Sets a list of strings [value] for the given [key] in the shared preferences.
-  ///
-  /// Example:
-  /// ```dart
-  /// await prefsService.setStringList('favorites', ['Apples', 'Bananas']);
-  /// ```
-  Future<void> setStringList(String key, List<String> value) async {
-    final prefs = await _prefs;
-    prefs.setStringList(key, value);
-  }
+  Future<void> setInt(String key, int value) async => _prefs.setInt(key, value);
 
-  /// Retrieves a boolean value for the given [key] from the shared preferences.
-  ///
-  /// Returns `null` if the key does not exist.
-  ///
-  /// Example:
-  /// ```dart
-  /// bool? isLoggedIn = await prefsService.getBool('isLoggedIn');
-  /// ```
-  Future<bool?> getBool(String key) async {
-    final prefs = await _prefs;
-    return prefs.getBool(key);
-  }
+  Future<void> setStringList(String key, List<String> value) async => _prefs.setStringList(key, value);
 
-  /// Retrieves a string value for the given [key] from the shared preferences.
-  ///
-  /// Returns `null` if the key does not exist.
-  ///
-  /// Example:
-  /// ```dart
-  /// String? username = await prefsService.getString('username');
-  /// ```
-  Future<String?> getString(String key) async {
-    final prefs = await _prefs;
-    return prefs.getString(key);
-  }
+  Future<bool?> getBool(String key) async => _prefs.getBool(key);
 
-  /// Retrieves an integer value for the given [key] from the shared preferences.
-  ///
-  /// Returns `null` if the key does not exist.
-  ///
-  /// Example:
-  /// ```dart
-  /// int? age = await prefsService.getInt('age');
-  /// ```
-  Future<int?> getInt(String key) async {
-    final prefs = await _prefs;
-    return prefs.getInt(key);
-  }
+  Future<String?> getString(String key) async => _prefs.getString(key);
 
-  /// Retrieves a list of strings for the given [key] from the shared preferences.
-  ///
-  /// Returns `null` if the key does not exist.
-  ///
-  /// Example:
-  /// ```dart
-  /// List<String>? favorites = await prefsService.getStringList('favorites');
-  /// ```
-  Future<List<String>?> getStringList(String key) async {
-    final prefs = await _prefs;
-    return prefs.getStringList(key);
-  }
+  Future<int?> getInt(String key) async => _prefs.getInt(key);
+
+  Future<List<String>?> getStringList(String key) async => _prefs.getStringList(key);
 }
