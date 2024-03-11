824c24b940aa8c84bf968758e844ba71a28ef700
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:00:55 -0500

    comment

--- benchmark/agbenchmark/challenges/verticals/code/3_file_organizer/custom_python/test.py
+++ benchmark/agbenchmark/challenges/verticals/code/3_file_organizer/custom_python/test.py
@@ -3,21 +3,3 @@ import subprocess
 import shutil
 import unittest
 
-class TestOrganizeFiles(unittest.TestCase):
-    def setUp(self):
-        # Create temporary directory
-        self.test_dir = tempfile.mkdtemp()
-
-        # File types and their corresponding directory
-        self.file_types = {
-            "test_image.png": "images",
-            "test_doc.txt": "documents",
-            "test_audio.mp3": "audio",
-        }
-
-        # Create test files
-        for file_name in self.file_types.keys():
-            file_path = os.path.join(self.test_dir, file_name)
-            with open(file_path, "w") as f:
-                f.write("Test content")
-
