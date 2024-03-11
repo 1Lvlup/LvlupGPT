defd1a5964e1df627dc78a9c17c90a10ecbcf674
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:39 -0500

    comment

--- frontend/lib/models/step_request_body.dart
+++ frontend/lib/models/step_request_body.dart
@@ -1,9 +1,20 @@
 class StepRequestBody {
+  // The `input` variable holds the primary input data of type String.
   final String? input;
+
+  // The `additionalInput` variable is an optional map that can store
+  // additional input data of type Map<String, dynamic>.
   final Map<String, dynamic>? additionalInput;
 
+  // The constructor `StepRequestBody` takes two parameters: `input` and
+  // `additionalInput`. The `input` parameter is required, while
+  // `additionalInput` is optional.
   StepRequestBody({required this.input, this.additionalInput});
 
+  // The `toJson` method converts the instance variables into a JSON-serializable
+  // Map<String, dynamic> object. If `input` is not null, it adds the key-value
+  // pair 'input': input to the map. If `additionalInput` is not null, it adds
+  // the key-value pair 'additional_input': additionalInput to the map.
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
     if (input != null) {
@@ -15,7 +26,8 @@ class StepRequestBody {
     return data;
   }
 
-  /// Returns `true` if both `input` and `additionalInput` are `null`.
+  // The `isEmpty` getter checks if both `input` and `additionalInput` are null.
+  // If both are null, it returns true; otherwise, it returns false.
   bool get isEmpty => input == null && additionalInput == null;
 }
 
