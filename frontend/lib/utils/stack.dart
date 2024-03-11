a0ebd28320704730db28dbe63c6e7582a599e5c2
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:38 -0500

    comment

--- frontend/lib/utils/stack.dart
+++ frontend/lib/utils/stack.dart
@@ -1,20 +1,35 @@
 class Stack<T> {
+  // A private list to store the elements in the stack
   final List<T> _list = [];
 
+  // Adds an element to the top of the stack
   void push(T element) {
+    // Add the element to the end of the list
     _list.add(element);
   }
 
+  // Removes and returns the top element from the stack
   T pop() {
+    // Get the last element in the list
     var element = _list.last;
+    
+    // Remove the last element from the list
     _list.removeLast();
+    
+    // Return the removed element
     return element;
   }
 
+  // Returns the top element of the stack without removing it
   T peek() {
+    // Return the last element in the list
     return _list.last;
   }
 
+  // Check if the stack is empty
   bool get isEmpty => _list.isEmpty;
+
+  // Check if the stack is not empty
   bool get isNotEmpty => _list.isNotEmpty;
 }
+
