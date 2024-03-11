bc47b51dfebf6cf194ffe3a9308dfdbd7e529266
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:37 -0500

    comment

--- frontend/lib/models/skill_tree/info.dart
+++ frontend/lib/models/skill_tree/info.dart
@@ -1,19 +1,24 @@
+// This class, `Info`, represents an object containing information about a certain topic.
 class Info {
+  // The `difficulty` field is a final variable that stores the level of difficulty
+  // associated with the information.
   final String difficulty;
+
+  // The `description` field is a final variable that stores a brief explanation
+  // or summary of the information.
   final String description;
+
+  // The `sideEffects` field is a final variable that stores a list of potential
+  // side effects or consequences related to the information.
   final List<String> sideEffects;
 
+  // The constructor for the `Info` class takes three required parameters:
+  // `difficulty`, `description`, and `sideEffects`. These parameters are used
+  // to initialize the corresponding final fields.
   Info({
     required this.difficulty,
     required this.description,
     required this.sideEffects,
   });
 
-  factory Info.fromJson(Map<String, dynamic> json) {
-    return Info(
-      difficulty: json['difficulty'] ?? "",
-      description: json['description'] ?? "",
-      sideEffects: List<String>.from(json['side_effects'] ?? []),
-    );
-  }
-}
+
