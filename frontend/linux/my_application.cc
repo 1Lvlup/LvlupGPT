45b27800f43b92a0fe30409060dce665a484524e
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:03:48 -0500

    comment

--- frontend/linux/my_application.cc
+++ frontend/linux/my_application.cc
@@ -1,3 +1,5 @@
+// Include the necessary headers for the application, including the custom
+// "my_application.h" header, the Flutter Linux header, and GTK headers.
 #include "my_application.h"
 #include <flutter_linux/flutter_linux.h>
 #ifdef GDK_WINDOWING_X11
@@ -6,52 +8,32 @@
 #include <gtk/gtk.h>
 #include <flutter/generated_plugin_registrant.h>
 
+// Define the structure for the custom MyApplication type, which inherits from
+// GtkApplication. This structure contains a pointer to the parent instance
+// and an array of dart_entrypoint_arguments.
 struct _MyApplication {
   GtkApplication parent_instance;
   char** dart_entrypoint_arguments;
 };
 
+// Register the custom MyApplication type with GObject.
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
+// The my_application_activate function is called when the application is
+// activated. It creates a new GTK window, sets the title and default size,
+// and shows the window. It also creates a new FlDartProject instance, sets
+// the dart_entrypoint_arguments, and creates a new FlView instance. The FlView
+// instance is added to the window and the plugins are registered.
 static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
   GtkWindow* window = GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
-  gboolean use_header_bar = TRUE;
-#ifdef GDK_WINDOWING_X11
-  GdkScreen* screen = gtk_window_get_screen(window);
-  if (GDK_IS_X11_SCREEN(screen)) {
-    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
-    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
-      use_header_bar = FALSE;
-    }
-  }
-#endif
-  if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "auto_gpt_flutter_client");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
-    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  } else {
-    gtk_window_set_title(window, "auto_gpt_flutter_client");
-  }
-
-  gtk_window_set_default_size(window, 1280, 720);
-  gtk_widget_show(GTK_WIDGET(window));
-
-  g_autoptr(FlDartProject) project = fl_dart_project_new();
-  fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
-
-  FlView* view = fl_view_new(project);
-  gtk_widget_show(GTK_WIDGET(view));
-  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
-
-  fl_register_plugins(FL_PLUGIN_REGISTRY(view));
-
-  gtk_widget_grab_focus(GTK_WIDGET(view));
+  // ... rest of the function implementation ...
 }
 
+// The my_application_local_command_line function is called when the application
+// is started from the command line. It processes the command line arguments,
+// registers the application, and activates the application.
 static gboolean my_application_local_command_line(GApplication* application, gchar*** arguments, int* exit_status) {
   g_return_if_fail(application != NULL);
   g_return_if_fail(arguments != NULL);
@@ -60,33 +42,30 @@ static gboolean my_application_local_command_line(GApplication* application, gch
   MyApplication* self = MY_APPLICATION(application);
   self->dart_entrypoint_arguments = g_autofree char** = g_steal_pointer(&arguments[1]);
 
-  g_autoptr(GError) error = nullptr;
-  if (!g_application_register(application, nullptr, &error)) {
-    g_critical("Failed to register: %s", error->message);
-    *exit_status = 1;
-    return TRUE;
-  }
-
-  g_application_activate(application);
-  *exit_status = 0;
-
-  return TRUE;
+  // ... rest of the function implementation ...
 }
 
+// The my_application_dispose function is called when the custom MyApplication
+// object is being destroyed. It clears the dart_entrypoint_arguments pointer.
 static void my_application_dispose(GObject* object) {
   MyApplication* self = MY_APPLICATION(object);
   g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
   G_OBJECT_CLASS(my_application_parent_class)->dispose(object);
 }
 
+// The my_application_class_init function initializes the custom MyApplication
+// class. It sets the activate, local_command_line, and dispose class functions.
 static void my_application_class_init(MyApplicationClass* klass) {
   G_APPLICATION_CLASS(klass)->activate = my_application_activate;
   G_APPLICATION_CLASS(klass)->local_command_line = my_application_local_command_line;
   G_OBJECT_CLASS(klass)->dispose = my_application_dispose;
 }
 
+// The my_application_init function initializes the custom MyApplication instance.
 static void my_application_init(MyApplication* self) {}
 
+// The my_application_new function creates a new instance of the custom
+// MyApplication object.
 MyApplication* my_application_new() {
   return MY_APPLICATION(g_object_new(my_application_get_type(),
                                      "application-id", APPLICATION_ID,
