80333780c54f0b87fc24f25fa7eba11a89c71b0c
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2024-03-10 19:01:23 -0500

    improve

--- frontend/lib/models/benchmark/benchmark_step_request_body.dart
+++ frontend/lib/models/benchmark/benchmark_step_request_body.dart
@@ -4,9 +4,18 @@ class BenchmarkStepRequestBody {
   BenchmarkStepRequestBody({required this.input});
 
   Map<String, dynamic> toJson() {
-    if (input == null) {
-      return {};
+    final jsonMap = <String, dynamic>{};
+    if (input != null) {
+      jsonMap['input'] = input;
     }
-    return {'input': input};
+    return jsonMap;
+  }
+
+  /// Constructs an instance of BenchmarkStepRequestBody from a JSON map.
+  factory BenchmarkStepRequestBody.fromJson(Map<String, dynamic> json) {
+    return BenchmarkStepRequestBody(
+      input: json['input'] as String?,
+    );
   }
 }
+
